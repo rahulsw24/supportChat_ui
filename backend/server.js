0bacb9ae1f8cd60ecb78958b48c55163adf5d604
Rahul Swarup <rahulsw24@gmail.com>
2025-05-22 17:00:52 +0530

    render commit

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+console.log("PORT from env:", process.env.PORT);
 app.get("/", (req, res) => {
   console.log(process.env.TOGETHER_API_KEY);
 });
@@ -25,6 +25,7 @@ app.post("/api/generate-reply", async (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
